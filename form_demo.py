f29eea78429918d999c690b5d486d7e8d5e9eec4
zondPZ <35355973+zondPZ@users.noreply.github.com>
2020-09-03 15:36:49 +0800

    Create form_demo.py

---
+++ form_demo.py
@@ -0,0 +1,152 @@
+import os
+import pandas as pd
+'''
+xls: pip install xlwt,xlrd
+xlsx: pip install openpyxl
+csv : 默认有 csv
+'''
+
+
+class BG():
+    def __init__(self):
+        pass
+
+    # 功能：分段
+    def FD_data(self,data_li, fd_len):
+        '''
+        :param data_li: 数据列表|list|例：[[1],[2],[3],[4]] or [{1},{2},{3},{4}]
+        :param fd_len: 分段长度|int|例：2
+        :return:  返回|list|例：[  [[1],[2]] , [[3],[4]] ]  or [ [{1},{2}] ,[{3},{4}] ]
+        '''
+        try:
+            new_li = []
+            for i in range(0, len(data_li), fd_len):
+                new_li.append(data_li[i:i + fd_len])
+            return new_li
+        except Exception as E:
+            print('分段失败：{}'.format(E))
+
+    # 数据处理
+    def CH(self, df, filename, header, mo):
+        '''
+        处理数据
+        :param df: df对象|
+        :param filename: 文件名|str|
+        :param header: 字段名|list|
+        :param mo: 打开文件方式|str|
+        :return:
+        '''
+
+        hz = os.path.splitext(filename)[1]
+        if hz == '.xlsx' or hz == '.xls':
+            if mo == 'a':
+                if not os.path.exists(filename):
+                    df.to_excel(filename, index=None, columns=header)
+                else:
+                    old_data = pd.read_excel(filename)
+                    new_data = pd.concat([old_data, df], axis=0)
+                    new_data.to_excel(filename, index=None)
+            elif mo == 'w':
+                df.to_excel(filename, index=None, columns=header)
+        elif hz == '.csv':
+            if mo == 'a':
+                if not os.path.exists(filename):
+                    df.to_csv(filename, index=False, mode=mo, header=header)
+                else:
+                    df.to_csv(filename, index=False, mode=mo, header=None)
+            elif mo == 'w':
+                df.to_csv(filename, index=False, mode=mo, header=header)
+
+    # 功能：将三种数据类型，转换成字典|
+    def To_dict(self, data):
+        '''
+        :param filename:文件名 |
+        :param data: 数据|{'字段名1':['第一行数据1','第二行数据1'],'字段名2':['第一行数据2','第二行数据2']} or
+                        [['字段名1','字段名2'],['第一行数据1','第二行数据1']] or
+                        [{'字段名1':'第一行数据1','字段名2':'第一行数据2'},{'字段名1':'第二行数据1','字段名2':'第二行数据2'}]
+        :return:[{'字段名1':'第一行数据1','字段名2':'第一行数据2'},{'字段名1':'第二行数据1','字段名2':'第二行数据2'}]
+        '''
+        if isinstance(data, dict):
+            if isinstance(list(data.values())[0], list):  # {'':[],'':[]}
+                header = list(data.keys())
+                dfs = pd.DataFrame.from_dict(data)
+                dfs.columns = header
+                news_data = list(dfs.values.tolist())
+                result = [dict(zip(header, d)) for d in news_data]
+                return result
+        elif isinstance(data, list):
+            if isinstance(data[0], list):  # [[],[]]
+                header = data[0]
+                dfs = pd.DataFrame(data[1:])
+                dfs.columns = header
+                news_data = list(dfs.values.tolist())
+                result = [dict(zip(header, d)) for d in news_data]
+                return result
+            elif isinstance(data[0], dict):  # [{},{}]
+                # header = data[0].keys()
+                # dfs=pd.DataFrame(data)
+                # dfs.columns=header
+                # news_data=list(dfs.values.tolist())
+                # result=[dict(zip(header,d)) for d in news_data]
+                # return result
+                return data
+
+    # 功能：保存数据的整个过程| 对数据进行处理成统一格式-》是否可以需要分段-》保存文件
+    def To_data(self,filename,data,mo,max_num=0):
+        '''
+        :param filename: 文件名|str|
+        :param data: 数据|list/dict|
+        :param mo: 模式|str|w:覆盖保存 ；a:追加
+        :param header:字段名|list|
+        :param max_num:最大分段长度|int|
+        :return:
+        ps: 1.xls单个sheet限制：65,535条记录 2.xlsx单个sheet限制：1,048,576条记录 3.csv 单个sheet无限制
+            因追加模式 a 分段不完善：对应格式限制数量以下的可以使用追加模式 a （不超过max_num）
+            超过对应格式限制数量的只能使用覆盖模式w
+        '''
+        if filename.find('/') != -1:
+            if not os.path.exists(filename[0:filename.rfind('/')]):
+                os.makedirs(filename[0:filename.rfind('/')])
+        if max_num == 0:
+            hz = os.path.splitext(filename)[1]
+            if hz == '.xls':
+                max_num = 65535
+            elif hz == '.xlsx':
+                max_num = 1048576
+            elif hz == '.csv':
+                max_num = 99999999999999999999999
+        new_data = self.To_dict(data)
+        header = new_data[0].keys()
+        if len(new_data) < max_num:  # 如果数据长度小于最大长度，直接保存文件
+            new_df = pd.DataFrame(new_data)
+            self.CH(new_df, filename, header, mo)
+        elif len(new_data) >max_num and mo != 'a':   # 如果数据长度大于最大长度，分段后保存文件(追加模式下不能分段)
+            FD_data = self.FD_data(new_data, max_num)
+            for fd_num in range(0, len(FD_data)):
+                new_filename = filename[0:filename.rfind('.')] + '-' + str(fd_num + 1) + filename[filename.rfind('.'):]  # xx.csv -> xx-1.csv
+                new_df = pd.DataFrame(FD_data[fd_num])
+                self.CH(new_df, new_filename, header, mo)
+        else:
+            print('{}|数据量超过{},不能使用a模式'.format(filename,max_num))
+
+
+
+
+if __name__ == '__main__':
+    # 测试数据
+    di={'name1':['aaa','bbb'],'age':['18','19']}
+    li=[['name','age'],['aaa','16'],['bbb','16'],['ccc','16']]
+    # dic=[{'name1':'aaa'},{'name1':'bbb'},{'name1':'ccc'},{'name1':'ddd'},{'name1':'sss'}]
+    dic=[{'name1':'aaa%d'%i} for i in range(0,65545)]
+    # 创建对象并写入
+    bg=BG()
+    for i in range(1,3):
+        # bg.To_data('xx/di.csv',di,'w')
+        # bg.To_data('xx/di.xls', di, 'w')
+        # bg.To_data('xx/di.xlsx', di, 'w')
+        # bg.To_data('xx/li.csv', li, 'w')
+        # bg.To_data('xx/li.xls', li, 'w')
+        # bg.To_data('xx/li.xlsx', li, 'w')
+        bg.To_data('xx/dic.csv', dic, 'w')
+        bg.To_data('xx/dic.xls', dic, 'w')
+        bg.To_data('xx/dic.xlsx', dic, 'w')
